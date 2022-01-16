a81f4bf408dd72a7778ef9e97063dd1b3e1df5ec
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-16 16:24:51 -0500

    A simple Zombie class

---
+++ CPP_Module_01/ex02/Zombie.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/16 00:58:32 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/16 00:59:41 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Zombie {
+private:
+	std::string	name;
+	std::string	type;
+public:
+	Zombie(std::string name, std::string type);
+	~Zombie();
+
+	const std::string&	get_name(void) const;
+	const std::string&	get_type(void) const;
+	void				set_name(std::string name);
+	void				set_type(std::string type);
+
+	void				announce(void) const;
+};
