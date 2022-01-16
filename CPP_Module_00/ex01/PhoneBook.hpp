3dde762c0a66ddcf47f9610e3c0c31faf9848527
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-16 09:14:45 -0500

    Updates getters to return const string

--- CPP_Module_00/ex01/PhoneBook.hpp
+++ CPP_Module_00/ex01/PhoneBook.hpp
@@ -13,6 +13,7 @@
 #pragma once
 
 #include "Contact.hpp"
+#include <iostream>
 
 class PhoneBook {
 private:
@@ -22,7 +23,7 @@ private:
 public:
 	PhoneBook() : size (0), index (0) {}
 	int 	get_size(void) const;
-	void	add_contact(std::string data[4], int number);
+	void	add_contact(std::string data[5]);
 	void	display_contact(int i) const;
 	void	display_phonebook(void) const;
 };
