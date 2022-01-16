e33ce81d82ef5add49a252f0b8545f016012f80e
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-16 09:13:15 -0500

    Included string library

--- CPP_Module_00/ex01/Contact.hpp
+++ CPP_Module_00/ex01/Contact.hpp
@@ -12,22 +12,24 @@
 
 #pragma once
 
+#include <string>
+
 class Contact {
 private:
-	std::string	_firstname;
-	std::string	_lastname;
-	std::string	_nickname;
-	std::string	_secret;
-	std::string	_number;
+	std::string	firstname;
+	std::string	lastname;
+	std::string	nickname;
+	std::string	secret;
+	std::string	number;
 public:
-	std::string&	get_firstname(void) const;
-	std::string&	get_lastname(void) const;
-	std::string&	get_nicktname(void) const;
-	std::string&	get_secret(void) const;
-	std::string&	get_number(void) const;
-	void			set_firstname(std:string firstname);
-	void			set_lasttname(std:string lastname);
-	void			set_nickname(std:string nickname);
-	void			set_secret(std:string secret);
-	void			set_number(srd::string number);
+	const std::string&	get_firstname(void) const;
+	const std::string&	get_lastname(void) const;
+	const std::string&	get_nickname(void) const;
+	const std::string&	get_secret(void) const;
+	const std::string&	get_number(void) const;
+	void				set_firstname(std::string firstname);
+	void				set_lastname(std::string lastname);
+	void				set_nickname(std::string nickname);
+	void				set_secret(std::string secret);
+	void				set_number(std::string number);
 };
