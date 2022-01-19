2195ce26942b0b1134862d8a733cbbdae26b3cf5
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-19 08:21:42 -0500

    Header of ZombieEvent

---
+++ CPP_Module_01/ex02/ZombieEvent.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieEvent.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 16:05:17 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/19 16:06:41 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Zombie.hpp"
+#include <cstdlib>
+
+class ZombieEvent {
+private:
+	std::string	type;
+	static std::string	namepool[7];
+public:
+	Zombie	*newZombie(std::string name) const;
+	Zombie	*randomChump(void) const;
+	void	setZombieType(std::string type);
+};
