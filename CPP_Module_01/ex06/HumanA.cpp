ffee6896d5826f9ad1475ebfd026e30384d9926b
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-20 15:41:17 -0500

    The HumanA class implementation

---
+++ CPP_Module_01/ex06/HumanA.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 00:13:15 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/20 00:14:26 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+
+HumanA::HumanA(std::string name, Weapon& weapon) : name(name), weapon(weapon) {}
+
+void	HumanA::attack() const
+{
+	std::cout << this->name << " attacks with his " << this->weapon.getType() << std::endl;
+}
