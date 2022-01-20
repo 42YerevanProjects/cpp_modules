6018c0d079383797a245705b68257cb1dddb1ea2
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-20 07:38:21 -0500

    ZombieHorde class

---
+++ CPP_Module_01/ex03/ZombieHorde.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieHorde.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 12:30:32 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/20 12:31:41 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Zombie.hpp"
+
+class ZombieHorde {
+private:
+	static std::string	namepool[7];
+	static std::string	typepool[7];
+	Zombie				*zombies;
+	int 				size;
+public:
+	ZombieHorde(int N);
+	~ZombieHorde(void);
+
+	void	announce(void) const;
+};
