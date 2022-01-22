513f29368330b70ef2d3fbf3ce262a32f248d5a0
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-22 11:35:10 -0500

    Class Fixed implementation

---
+++ CPP_Module_02/ex00/Fixed.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 20:10:23 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/22 19:11:10 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->value = 0;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed& other)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->setRawBits(other.getRawBits());
+	return (*this);
+}
+
+int		Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	this->value = raw;
+}
