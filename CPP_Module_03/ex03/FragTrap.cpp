1b80133a141b993c080668dcc0658c5685e9af3c
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-25 13:11:31 -0500

    FragTrap class interface implementation

---
+++ CPP_Module_03/ex03/FragTrap.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 19:55:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/25 20:01:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void) : ClapTrap()
+{
+	std::cout << "FragTrap default constructor has been called!" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string& name) : ClapTrap(name)
+{
+	std::cout << "<FragTrap " << this->name << "> Status: Created" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
+{
+	std::cout << "<FragTrap copy-constructor has been called!> Status: Cloned" << std::endl;
+}
+
+FragTrap::~FragTrap(void)
+{
+	std::cout << "<FragTrap " << this->name << "> Status: Dead" << std::endl;
+}
+
+void	FragTrap::highFivesGuys(void) const
+{
+	std::cout << "<FragTrap " << this->name << "> High Five Guys! Status: Friendly" << std::endl;
+}
