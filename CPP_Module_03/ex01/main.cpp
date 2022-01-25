fddc398ecbbc6d723cfc60750883e239a566aa46
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-25 09:02:53 -0500

    Test program for ScavTrap

---
+++ CPP_Module_03/ex01/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 13:43:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/25 13:45:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int	main()
+{
+	ScavTrap	temp("Daniel");
+	ScavTrap	Daniel;
+
+	/* Assignment check */
+	temp.setAttackDamage(55);
+	Daniel = temp;
+
+	/* Info */
+	std::cout << "\n---------- ScavTrap Daniel ----------" << std::endl;
+	std::cout << "Hitpoints: " << Daniel.getHitPoints() << " ";
+	std::cout << "Energy: " << Daniel.getEnergyPoints() << " ";
+	std::cout << "Attack Damage: " << Daniel.getAttackDamage() << " ";
+	std::cout << "Status: Active" << std::endl;
+
+	/* Test */
+	std::cout << "\nStart attacking...\n" << std::endl;
+
+	Daniel.attack("Sharks");
+	Daniel.attack("Pirates");
+	Daniel.takeDamage(3);
+	Daniel.takeDamage(14);
+	Daniel.beRepaired(10);
+	Daniel.attack("Dragon");
+	Daniel.attack("Spirits");
+	Daniel.attack("Lions");
+	Daniel.attack("Kings");
+
+	std::cout << "\nFights are over..." << std::endl;
+}
