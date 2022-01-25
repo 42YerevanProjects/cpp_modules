0c0a379645828bbc3b8595129580096e4324fe7e
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-25 11:18:24 -0500

    FragTrap class interface

---
+++ CPP_Module_03/ex02/FragTrap.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 19:49:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/25 19:57:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+	public:
+		FragTrap(const std::string& name);
+		FragTrap(const FragTrap& other);
+		FragTrap(void);
+		~FragTrap(void);
+
+		using   ClapTrap::operator=;
+		void	highFivesGuys(void) const;
+};
