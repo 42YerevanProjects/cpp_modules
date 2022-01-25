9039442c7d6c5a3023c31044ec1f60293df19238
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-25 13:12:22 -0500

    DiamondTrap class interface implementation

---
+++ CPP_Module_03/ex03/DiamondTrap.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 21:09:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/25 21:18:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap(void) : ClapTrap(), ScavTrap(), FragTrap()
+{
+	std::cout << "<DiamondTrap default constructor has been called!" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const std::string& name)
+	: ClapTrap(name + "_clap_name"), ScavTrap(name + "_clap_name"), FragTrap(name + "_clap_name")
+{
+	std::cout << "<DiamondTrap " << this->name << " Status: Created" << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap& other) 
+	: ClapTrap(other.name + "_clap_trap"), ScavTrap(other.name + "_clap_trap"), FragTrap(other.name + "_clap_trap")
+{
+	std::cout << "<DiamondTrap copy-constructor has been called!> Status: Cloned" << std::endl;
+	this->name = other.name;
+}
+
+DiamondTrap::~DiamondTrap(void)
+{
+	std::cout << "<DiamondTrap " << this->name << "> Status: Dead" << std::endl;
+}
+
+void	DiamondTrap::whoAmI(void)
+{
+	std::cout << "<DiamondTrap " << this->name << "> DiamondTrap->name: " << this->name << std::endl;
+	std::cout << "[DiamondTrap " << this->name << "> ClapTrap->name: " << ClapTrap::name << std::endl;
+}
