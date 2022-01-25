d4f5a2ea63921c57dd0276abfc75db2bf4ef6821
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-25 13:12:03 -0500

    DiamondTrap class interface

---
+++ CPP_Module_03/ex03/DiamondTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 21:04:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/25 15:10:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+class DiamondTrap : public ScavTrap, FragTrap {
+	private:
+		std::string	name;
+	public:
+		DiamondTrap(const std::string& name);
+		DiamondTrap(const DiamondTrap& other);
+		DiamondTrap(void);
+		~DiamondTrap(void);
+
+		using	ClapTrap::operator=;
+		using	ScavTrap::attack;
+		void	whoAmI(void);
+};
+
