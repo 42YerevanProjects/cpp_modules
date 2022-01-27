c9ea5bdb1946f88d80c56df4eadf0d1c071dfe53
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-27 15:07:44 -0500

    Class Cat interface

---
+++ CPP_Module_04/ex02/Cat.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 16:22:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/27 19:29:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Cat : public Animal {
+	private:
+		Brain	*brain;
+	public:
+		Cat(void);
+		Cat(const Cat& other);
+		~Cat();
+
+		Cat&	operator=(const Cat& other);
+		Animal&	operator=(const Animal& other);
+		void	makeSound(void) const;
+		Brain	*getBrain(void) const;
+};
+
