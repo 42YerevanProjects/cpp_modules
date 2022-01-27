e0026db548fba858e0acda08c999631bf9b08392
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-27 14:05:33 -0500

    Test program for Animals with brain

---
+++ CPP_Module_04/ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 16:35:27 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/27 16:37:35 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+#define NUMBER_OF_ANIMALS 8
+
+int main()
+{
+	Animal	*animals[NUMBER_OF_ANIMALS];
+	Brain	*brain;
+
+	std::cout << "\nCreatig Animals' array\n" << std::endl;
+	for (int i = 0; i < NUMBER_OF_ANIMALS; i++)
+	{
+		if (i % 2 == 0)
+			animals[i] = new Dog();
+		else
+			animals[i] = new Cat();
+		std::cout << std::endl;
+	}
+
+	std::cout << "Assigning some ideas ti animal[7]" <<std::endl;
+	brain = animals[7]->getBrain();
+	brain->addIdea("I'm hungry");
+	brain->addIdea("That's a strange idea I'm having");
+	brain->addIdea("Ball!!!!!");
+	brain->addIdea("Squirrel!!!!!");
+	std::cout << "First idea of animal[7]: " << brain->getIdea(0) << std::endl;
+
+	std::cout << "\nAssigning animal[7] to animal[5]" << std::endl;
+	*(animals[5]) = *(animals[7]);
+	std::cout << "First idea of animal[5]: " << animals[5]->getBrain()->getIdea(0) << std::endl;
+
+	std::cout << "\nDestructing Animals\n" << std::endl;
+	for (int i = 0; i < NUMBER_OF_ANIMALS; i++)
+		delete animals[i];
+}
