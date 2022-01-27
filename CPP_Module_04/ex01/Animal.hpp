df85894eda7da72ed54c5a13eb47daee15fbf4d0
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-27 14:03:09 -0500

    Animal class interface

---
+++ CPP_Module_04/ex01/Animal.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 15:08:08 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/27 15:12:35 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Brain.hpp"
+
+class Animal {
+	protected:
+		std::string	type;
+		Animal(const std::string& type);
+	public:
+		Animal(void);
+		Animal(const Animal& other);
+		virtual ~Animal(void);
+
+		virtual Animal&				operator=(const Animal& other);
+		const std::string&	getType(void) const;
+		void				setType(const std::string& type);
+		virtual void		makeSound(void) const;
+		virtual Brain		*getBrain(void) const = 0;
+};
