30eff4ffb94dad4b128c7fecdf74b04963257197
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-27 14:03:39 -0500

    Dog class interface implementation

---
+++ CPP_Module_04/ex01/Dog.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 16:11:27 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/27 16:14:35 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+Dog::Dog(void) : Animal("Dog")
+{
+	std::cout << "Default constructor of " << this->type << std::endl;
+	this->brain = new Brain;
+}
+
+Dog::Dog(const Dog& other) : Animal("Dog")
+{
+	std::cout << "Copy-Constructor of " << this->type << std::endl;
+	*this = other;
+}
+
+Dog::~Dog(void)
+{
+	std::cout << this->type << " has been destroyed" << std::endl;
+	delete this->brain;
+}
+
+Animal&		Dog::operator=(const Animal& other)
+{
+	const Dog	*pOther; 
+
+	pOther = dynamic_cast<const Dog*>(&other);
+	if(pOther)
+	{
+		this->type = pOther->type;
+		*this->brain = *pOther->brain;
+	}
+
+	return (*this);
+}
+
+void	Dog::makeSound(void) const
+{
+	std::cout << "* Barking! *" << std::endl;
+}
+
+Brain*	Dog::getBrain(void) const
+{
+	return (this->brain);
+}
