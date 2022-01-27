e0742a1b3b3a3ab19dfa499d31441c8fad509ffd
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-27 14:03:28 -0500

    Dog class interface

---
+++ CPP_Module_04/ex01/Dog.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 16:02:18 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/27 16:10:53 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal {
+	private:
+		Brain	*brain;
+	public:
+		Dog(void);
+		Dog(const Dog& other);
+		~Dog();
+
+		Animal&	operator=(const Animal& other);
+		void	makeSound(void) const;
+		Brain	*getBrain(void) const;
+};
