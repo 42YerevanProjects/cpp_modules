93156f3cc1ec8e6cdb82e90a1b4a2b874df3ca40
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-29 18:14:43 -0500

    Test program

---
+++ CPP_Module_05/ex02/main.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 02:12:17 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/30 02:20:29 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+int main()
+{
+	Form			*form = NULL;
+	Bureaucrat		bob("bob", 1);
+	Bureaucrat		phil("phil", 40);
+	Bureaucrat		luc("luc", 150);
+
+	try
+	{
+		form = new PresidentialPardonForm("28Z");
+		form->execute(bob);
+		delete form;
+		form = NULL;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		form = new PresidentialPardonForm("28A");
+		form->beSigned(bob);
+		form->execute(bob);
+		delete form;
+		form = NULL;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	try
+	{
+		form = new RobotomyRequestForm("28B");
+		form->beSigned(bob);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		form->execute(phil);
+		delete form;
+		form = NULL;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+	try
+	{
+		form = new ShrubberyCreationForm("28C");
+		form->beSigned(bob);
+		form->execute(phil);
+		form->execute(luc);
+		delete form;
+		form = NULL;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
