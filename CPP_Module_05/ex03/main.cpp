18ed0dad1f3986aba56a2de624a57c0dc5fe79a0
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-30 05:05:49 -0500

    Test program

---
+++ CPP_Module_05/ex03/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 13:52:24 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/30 13:58:21 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Intern.hpp"
+#include <ctime>
+
+int main()
+{
+	Intern	intern;
+	Form	*f;
+
+	f = intern.makeForm("shrubbery creation", "28C");
+	std::cout << *f << std::endl;
+	std::cout << std::endl;
+
+
+	f = intern.makeForm("robotomy request", "28B");
+	std::cout << *f << std::endl;
+	std::cout << std::endl;
+
+	f = intern.makeForm("presidential pardon", "28A");
+	std::cout << *f << std::endl;
+	std::cout << std::endl;
+	
+	f = intern.makeForm("undefined", "28A");
+
+	return 0;
+}
