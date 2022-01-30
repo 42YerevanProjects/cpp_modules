c99be9cdb7540bc2e429581294feacf525fa6591
Nathanael <sargishovsepyan2001@gmail.com>
2022-01-30 03:31:20 -0500

    Updated

--- CPP_Module_05/ex02/main.cpp
+++ CPP_Module_05/ex02/main.cpp
@@ -17,71 +17,66 @@
 
 int main()
 {
+	Bureaucrat		Odin("Odin", 1);
+	Bureaucrat		Thor("Thor", 40);
+	Bureaucrat		Loki("Loki", 150);
 	Form			*form = NULL;
-	Bureaucrat		bob("bob", 1);
-	Bureaucrat		phil("phil", 40);
-	Bureaucrat		luc("luc", 150);
 
+	std::cout << "Let's execute an unsigned presidential pardon form" << std::endl;
 	try
 	{
 		form = new PresidentialPardonForm("28Z");
-		form->execute(bob);
+		form->execute(Odin);
 		delete form;
 		form = NULL;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Exception: " << e.what() << std::endl;
 	}
 
+	std::cout << "\nLet's sign and execute a presidential pardon form" << std::endl;
 	try
 	{
 		form = new PresidentialPardonForm("28A");
-		form->beSigned(bob);
-		form->execute(bob);
+		form->beSigned(Odin);
+		form->execute(Odin);
 		delete form;
 		form = NULL;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Exception: " << e.what() << std::endl;
 	}
 
-
+	std::cout << "\nLet's sign and execute robotomy request form with different people" << std::endl;
 	try
 	{
 		form = new RobotomyRequestForm("28B");
-		form->beSigned(bob);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
-		form->execute(phil);
+		Odin.signForm(*form);
+		std::cout << std::endl;
+		Thor.executeForm(*form);
 		delete form;
 		form = NULL;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Exception: " << e.what() << std::endl;
 	}
 
-
+	std::cout << "\nLet's execute shrubbery creation form with unauthorized bureaucrat" << std::endl;
 	try
 	{
 		form = new ShrubberyCreationForm("28C");
-		form->beSigned(bob);
-		form->execute(phil);
-		form->execute(luc);
+		Odin.signForm(*form);
+		Thor.executeForm(*form);
+		Loki.executeForm(*form);
 		delete form;
 		form = NULL;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Exception: " << e.what() << std::endl;
 	}
 
 	return 0;
