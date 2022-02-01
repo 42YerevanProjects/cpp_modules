2ecd6f631b672c197ff6b3d09b84f31564355045
Nathanael <sargishovsepyan2001@gmail.com>
2022-02-01 05:09:58 -0500

    main.cpp

---
+++ CPP_Module_06/ex00/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 21:14:17 by shovsepy          #+#    #+#             */
+/*   Updated: 2022/01/31 21:20:29 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "TypeAnalyst.hpp"
+
+int	main(int argc, char **argv)
+{
+	TypeAnalyst	*analyst;
+
+	if (argc != 2)
+	{
+		std::cout << "THe program takes a string literal as an input!" << std::endl;
+		return (1);
+	}
+
+	try
+	{
+		analyst = new TypeAnalyst(argv[1]);	
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+		exit(0);
+	}
+
+	analyst->printCharType();
+	analyst->printIntType();
+	analyst->printFloatType();
+	analyst->printDoubleType();
+
+	delete analyst;
+}
+
